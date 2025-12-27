fa3ce9f4d888aea05b0a25c95edef0b4439cc809
karan <karanpachore987@gmail.com>
2025-12-28 00:51:01 +0530

    initial instance creation

---
+++ file.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+
+resource "aws_instance" "myinstance" {
+
+    ami = "ami-087d1c9a513324697"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = ["sg-00bdd2050b067edca"]
+    key_name = "t1-key.pem"
+    availability_zone = "ap-south-1a"
+
+    tags = {
+      name = "myinstance"
+    }
+  
+}
