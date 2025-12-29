594b7be47d896c9947e13d690044b1c43041406a
karan <karanpachore987@gmail.com>
2025-12-29 16:16:11 +0530

    initial instance creation

--- file.tf
+++ file.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "myinstance" {
     ami = "ami-087d1c9a513324697"
     instance_type = "t3.micro"
     vpc_security_group_ids = ["sg-00bdd2050b067edca"]
-    key_name = "t1-key.pem"
+    key_name = "t1-key"
     availability_zone = "ap-south-1a"
 
     tags = {
