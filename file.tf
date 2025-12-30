c2cff470e527ce69eb17246e88520f6a17fd3466
karan <karanpachore987@gmail.com>
2025-12-31 00:49:34 +0530

    output block added

--- file.tf
+++ file.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "myinstance1" {
   }
 
   output "instance_public_ip" {
-    value = aws_instance.myinstance1.public.ip
+    value =aws_instance.myinstance1
     
   }
   output "instance_id" {
