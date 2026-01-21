c125820d5d8aa8872f48d0125a50b372c2f094a1
karan <karanpachore987@gmail.com>
2026-01-21 23:43:28 +0530

    apache2 created

--- Terraform/modules/instance/variable.tf
+++ Terraform/modules/instance/variable.tf
@@ -1,12 +1,5 @@
-variable "ami_id" {
-    
-  }
-
-  variable "instance_type" {
-    
-  }
-  variable "key_name" {
-    
-  }
-  variable "vpc_id" {
-  }
+# variable "ami_id" {}
+# variable "instance_type" {}
+# variable "key_name" {}
+# variable "vpc_id" {}
+# variable "tags" {}
