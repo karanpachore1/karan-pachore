df38b07042df82ef513c60fcab6ef2cb7748cd9f
karan <karanpachore987@gmail.com>
2026-02-25 00:29:56 +0530

    terraform project file added

--- Terraform/modules/instance/main.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_instance" "myinstance" {
-#   ami = var.ami_id
-#   instance_type = var.instance_type
-#   vpc_security_group_ids = [data.aws_security_group.mysg3.id]
-#   key_name = var.key_name
-#   tags = {
-#     Name = var.tags
-#   }
-# }
