75dc45d0a4c9267ae9dfe29a9063bebe2cfc8d33
karan <karanpachore987@gmail.com>
2026-02-26 10:26:00 +0530

    terraform project file added

--- Terraform/Terraform_AWS_Project/ec2.tf
+++ Terraform/Terraform_AWS_Project/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web_server" {
-  ami           = "ami-0a145236ee857bedd"  # Amazon Linux 2 (UK)
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.web_sg.id]
+  ami                    = "ami-0a145236ee857bedd"
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "Terraform-EC2"
